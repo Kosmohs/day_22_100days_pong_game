405b97d60bbaac31f9dd5ec5b942a6fccbecbc21
Konstantin <konstantin_n.v@mail.ru>
2023-03-17 12:46:52 +0600

    first commit

---
+++ ball.py
@@ -0,0 +1,33 @@
+from turtle import Turtle, Screen
+
+class Ball(Turtle):
+  def __init__(self):
+    super().__init__()
+    self.shape("circle")
+    self.color("yellow")
+    self.penup()
+    self.x_move = 10
+    self.y_move = 10
+    self.move_speed = 0.1
+
+  def move(self):
+    new_x = self.xcor() + self.x_move
+    new_y = self.ycor() + self.y_move
+    self.goto(new_x, new_y)
+
+  def bounce_y(self):
+        self.y_move *= -1
+
+  def bounce_x(self):
+        self.x_move *= -1
+        self.move_speed *= 0.9
+
+  def reset_position(self):
+    self.hideturtle()
+    self.goto(0, 0)
+    self.move_speed = 0.1
+    self.showturtle()
+    self.bounce_x()
+        
+ 
+  
---
+++ main.py
@@ -0,0 +1,55 @@
+from turtle import Screen, Turtle
+from paddle import Paddle
+from ball import Ball
+from scoreboard import Scoreboard
+import time 
+
+screen = Screen()
+screen.bgcolor("black")
+screen.setup(800, 600)
+screen.title("Pong")
+screen.tracer(0)
+
+r_paddle = Paddle((350, 250))
+l_paddle = Paddle((-350, 0))
+ball = Ball()
+scoreboard = Scoreboard()
+
+screen.tracer(1)
+ball.goto(300, 280)
+
+screen.listen()
+screen.onkey(r_paddle.go_up, "Up")
+screen.onkey(r_paddle.go_down, "Down")
+
+screen.onkey(l_paddle.go_up, "w")
+screen.onkey(l_paddle.go_down, "s")
+
+
+game_is_on = True
+while game_is_on:
+  time.sleep(ball.move_speed)
+  screen.update()
+  ball.move()
+
+  # Detecting collision with wall
+  if ball.ycor() > 280 or ball.ycor() < -280:
+    ball.bounce_y()
+#   elif ball.xcor() > 380 or ball.xcor() < -380:
+#     ball.bounce_x()
+
+  # Detect collision with paddles
+  if ball.distance(r_paddle) < 50 and ball.xcor() > 320 or ball.distance(l_paddle) < 50 and ball.xcor() < -320:
+    ball.bounce_x()
+
+  # Detect r_paddle miss
+  if ball.xcor() > 380:
+    ball.reset_position()
+    scoreboard.l_point()
+
+  # Detect l_paddle miss
+  if ball.xcor() < -380:
+    ball.reset_position()
+    scoreboard.r_point()
+
+screen.exitonclick()
